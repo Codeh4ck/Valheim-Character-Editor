5ebe172a3b9130cb2ee662a301d7014e932a8618
Nikolas Andreou <nickandreou94@gmail.com>
2021-06-28 18:24:23 +0300

    Add project files.

---
+++ ValheimCharacterEditor/Forms/FrmMain.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace ValheimCharacterEditor
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ValheimCharacterEditor/Valheim/GameObjectItemProperties.cs
@@ -0,0 +1,34 @@
+﻿namespace ValheimCharacterEditor.Valheim
+{
+    public class GameObjectItemProperties
+    {
+        public int MaxStackSize { get; set; }
+        public int MaxDurability { get; set; }
+        public int DurabilityPerLevel { get; set; }
+        public int MaxQuality { get; set; }
+        public int MaxVariants { get; set; }
+        public bool Craftable { get; set; }
+        public string DisplayName { get; set; }
+
+        public GameObjectItemProperties() { }
+
+        public GameObjectItemProperties(
+            int maxStackSize,
+            int maxDurability,
+            int durabilityPerLevel,
+            int maxQuality,
+            int maxVariants,
+            bool craftable,
+            string displayName)
+        {
+
+            MaxStackSize = maxStackSize;
+            MaxDurability = maxDurability;
+            DurabilityPerLevel = durabilityPerLevel;
+            MaxQuality = maxQuality;
+            MaxVariants = maxVariants;
+            Craftable = craftable;
+            DisplayName = displayName;
+        }
+    }
+}
