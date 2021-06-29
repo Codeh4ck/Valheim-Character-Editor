f1b5375db050e241c3144b7e32c9f21c4b595e9e
Nikolas Andreou <nickandreou94@gmail.com>
2021-06-29 21:20:37 +0300

    Prevent app from closing until the last form is closed

---
+++ ValheimCharacterEditor/LastFormClosingApplicationContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ValheimCharacterEditor
+{
+    public class LastFormClosingApplicationContext : ApplicationContext
+    {
+        public LastFormClosingApplicationContext(Form mainForm) : base(mainForm) { }
+
+        protected override void OnMainFormClosed(object sender, EventArgs e)
+        {
+            for (int i = Application.OpenForms.Count - 1; i >= 0; i--)
+            {
+                Form currentForm = Application.OpenForms[i];
+
+                if (ReferenceEquals(currentForm, MainForm)) continue;
+
+                MainForm = currentForm;
+                return;
+            }
+
+            base.OnMainFormClosed(sender, e);
+        }
+    }
+}
