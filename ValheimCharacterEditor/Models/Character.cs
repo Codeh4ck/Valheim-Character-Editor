c5c884a8b769f97e87166e71181407600572ab14
Nikolas Andreou <nickandreou94@gmail.com>
2021-06-29 21:22:40 +0300

    Added a form to begin working on the inventory UI

---
+++ ValheimCharacterEditor/Models/Character.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using ValheimCharacterEditor.Valheim;
+
+namespace ValheimCharacterEditor.Models
+{
+    public class CharacterEntity
+    {
+        public string File;
+        public Character Data;
+    }
+}
