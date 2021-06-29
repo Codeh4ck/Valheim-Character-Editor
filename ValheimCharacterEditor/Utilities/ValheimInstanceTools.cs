d63616ff0ba2bcdecdb662a477ac2e8aded49d2f
Nikolas Andreou <nickandreou94@gmail.com>
2021-06-29 21:22:05 +0300

    Added helper class to manage Valheim if it's running

---
+++ ValheimCharacterEditor/Utilities/ValheimInstanceTools.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+namespace ValheimCharacterEditor.Utilities
+{
+    public class ValheimInstanceTools
+    {
+        public static bool IsValheimRunning()
+        {
+            Process[] processes = Process.GetProcessesByName("valheim");
+            return processes.Length > 0;
+        }
+
+        public static bool TryKillValheim()
+        {
+            Process[] processes = Process.GetProcessesByName("valheim");
+            if (processes.Length == 0) return false;
+            
+            try
+            {
+                foreach (Process process in processes)
+                    process.Kill();
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
