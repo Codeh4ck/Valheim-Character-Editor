7bae0bbd9f96d2dd34918287de2ccd05fab1b760
Nikolas Andreou <nickandreou94@gmail.com>
2021-07-03 06:32:15 +0300

    Finished item editor UI & functionality

---
+++ ValheimCharacterEditor/Forms/FrmItemEditor.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace ValheimCharacterEditor.Forms
+{
+    partial class FrmItemEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbItemImage = new System.Windows.Forms.PictureBox();
+            this.cbItems = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnApply = new System.Windows.Forms.Button();
+            this.numQuantity = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numQuality = new System.Windows.Forms.NumericUpDown();
+            this.numDurability = new System.Windows.Forms.Label();
+            this.numVariant = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pbItemImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numQuantity)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numQuality)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numVariant)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbItemImage
+            // 
+            this.pbItemImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbItemImage.Location = new System.Drawing.Point(12, 12);
+            this.pbItemImage.Name = "pbItemImage";
+            this.pbItemImage.Size = new System.Drawing.Size(64, 64);
+            this.pbItemImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbItemImage.TabIndex = 1;
+            this.pbItemImage.TabStop = false;
+            // 
+            // cbItems
+            // 
+            this.cbItems.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbItems.FormattingEnabled = true;
+            this.cbItems.Location = new System.Drawing.Point(98, 33);
+            this.cbItems.Name = "cbItems";
+            this.cbItems.Size = new System.Drawing.Size(317, 21);
+            this.cbItems.TabIndex = 2;
+            this.cbItems.SelectedIndexChanged += new System.EventHandler(this.cbItems_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(95, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Item:";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(219, 142);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(95, 25);
+            this.btnCancel.TabIndex = 39;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnApply
+            // 
+            this.btnApply.Location = new System.Drawing.Point(320, 142);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(95, 25);
+            this.btnApply.TabIndex = 38;
+            this.btnApply.Text = "Apply";
+            this.btnApply.UseVisualStyleBackColor = true;
+            this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
+            // 
+            // numQuantity
+            // 
+            this.numQuantity.Enabled = false;
+            this.numQuantity.Location = new System.Drawing.Point(98, 84);
+            this.numQuantity.Name = "numQuantity";
+            this.numQuantity.Size = new System.Drawing.Size(91, 21);
+            this.numQuantity.TabIndex = 40;
+            this.numQuantity.ValueChanged += new System.EventHandler(this.numQuantity_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(95, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 41;
+            this.label2.Text = "Quantity:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(208, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 43;
+            this.label3.Text = "Quality:";
+            // 
+            // numQuality
+            // 
+            this.numQuality.Enabled = false;
+            this.numQuality.Location = new System.Drawing.Point(211, 84);
+            this.numQuality.Name = "numQuality";
+            this.numQuality.Size = new System.Drawing.Size(91, 21);
+            this.numQuality.TabIndex = 42;
+            this.numQuality.ValueChanged += new System.EventHandler(this.numQuality_ValueChanged);
+            // 
+            // numDurability
+            // 
+            this.numDurability.AutoSize = true;
+            this.numDurability.Location = new System.Drawing.Point(321, 63);
+            this.numDurability.Name = "numDurability";
+            this.numDurability.Size = new System.Drawing.Size(52, 13);
+            this.numDurability.TabIndex = 45;
+            this.numDurability.Text = "Variant:";
+            // 
+            // numVariant
+            // 
+            this.numVariant.Enabled = false;
+            this.numVariant.Location = new System.Drawing.Point(324, 84);
+            this.numVariant.Name = "numVariant";
+            this.numVariant.Size = new System.Drawing.Size(91, 21);
+            this.numVariant.TabIndex = 44;
+            this.numVariant.ValueChanged += new System.EventHandler(this.numVariant_ValueChanged);
+            // 
+            // FrmItemEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 179);
+            this.Controls.Add(this.numDurability);
+            this.Controls.Add(this.numVariant);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numQuality);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numQuantity);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnApply);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbItems);
+            this.Controls.Add(this.pbItemImage);
+            this.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FrmItemEditor";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Edit item";
+            this.Load += new System.EventHandler(this.FrmItemEditor_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbItemImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numQuantity)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numQuality)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numVariant)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbItemImage;
+        private System.Windows.Forms.ComboBox cbItems;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnApply;
+        private System.Windows.Forms.NumericUpDown numQuantity;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numQuality;
+        private System.Windows.Forms.Label numDurability;
+        private System.Windows.Forms.NumericUpDown numVariant;
+    }
+}
---
+++ ValheimCharacterEditor/Forms/FrmItemEditor.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using ValheimCharacterEditor.Utilities.Extensions;
+using ValheimCharacterEditor.Valheim;
+
+namespace ValheimCharacterEditor.Forms
+{
+    public partial class FrmItemEditor : Form
+    {
+        private Item _item;
+        private readonly CharacterEntity _character;
+        private bool _initializedFirstItem;
+
+        private FrmItemEditor(Item item, CharacterEntity character)
+        {
+            if (character == null) throw new ArgumentNullException(nameof(character));
+            
+            _item = item;
+            _character = character;
+
+            InitializeComponent();
+        }
+
+        public static Item GetItemModification(Form parent, Item item, CharacterEntity character)
+        {
+            using FrmItemEditor itemEditor = new FrmItemEditor(item, character);
+            
+            if (itemEditor.ShowDialog(parent) != DialogResult.OK)
+                return null;
+
+            return itemEditor._item;
+        }
+
+        private void FrmItemEditor_Load(object sender, EventArgs e)
+        {
+            // All non-items generally contain an underscore
+            // Any item that contains a digit is usually some character appearance customization
+
+            cbItems.DataSource = GameObjects.ItemProperties.Values
+                .Select(x => x.DisplayName)
+                .Where(x => !x.Contains("_") && !x.Any(char.IsDigit))
+                .OrderBy(x => x)
+                .ToList();
+
+            if (_item != null)
+            {
+                LoadItem(_item);
+
+                GameObjectProperties itemProperties = GameObjects.ItemProperties[_item.Name];
+                cbItems.SelectedIndex = cbItems.FindStringExact(itemProperties.DisplayName);
+
+                _initializedFirstItem = true;
+            }
+            else
+            {
+                cbItems.SelectedIndex = 0;
+            }
+        }
+
+        private void cbItems_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            string selectedItem = cbItems.SelectedItem.ToString();
+
+            if (string.IsNullOrEmpty(selectedItem)) return;
+
+            KeyValuePair<string ,GameObjectProperties> gameObject =
+                GameObjects.ItemProperties.FirstOrDefault(x => x.Value.DisplayName == selectedItem);
+
+            if (_item == null || _initializedFirstItem)
+            {
+                Item item = new Item
+                {
+                    Name = gameObject.Key,
+                    Quality = gameObject.Value.MaxQuality,
+                    Variant = 0,
+                    Stack = 1,
+                    CrafterId = _character.Data.Id,
+                    CrafterName = _character.Data.Name,
+                };
+
+
+                item.Durability = item.GetMaxDurability(gameObject.Value.MaxQuality);
+
+                _item = item;
+            }
+
+            LoadItem(_item);
+
+            Image image = Image.FromFile(_item.GetImagePath());
+            pbItemImage.Image = image;
+        }
+
+        private void LoadItem(Item item)
+        {
+            if (!GameObjects.ItemProperties.ContainsKey(item.Name))
+            {
+                numQuality.Value = 1;
+                numQuality.Enabled = false;
+                numQuantity.Value = 1;
+                numQuantity.Enabled = false;
+                numVariant.Value = 0;
+                numVariant.Enabled = false;
+
+                return;
+            }
+
+            GameObjectProperties itemProperties = GameObjects.ItemProperties[item.Name];
+
+            if (itemProperties.MaxQuality > 1)
+            {
+                numQuality.Enabled = true;
+                numQuality.Maximum = itemProperties.MaxQuality;
+                numQuality.Value = item.Quality == 0 ? 1 : item.Quality;
+            }
+            else
+            {
+                numQuality.Enabled = false;
+                numQuality.Value = _item.Quality;
+            }
+
+            if (itemProperties.MaxStackSize > 1)
+            {
+                numQuantity.Enabled = true;
+                numQuantity.Maximum = itemProperties.MaxStackSize;
+                numQuantity.Value = item.Stack == 0 ? 1 : item.Stack; ;
+            }
+            else
+            {
+                numQuantity.Enabled = false;
+                numQuantity.Value = _item.Stack;
+            }
+
+            if (itemProperties.MaxVariants > 0)
+            {
+                numVariant.Enabled = true;
+                numVariant.Maximum = itemProperties.MaxVariants - 1;
+                numVariant.Value = item.Variant;
+            }
+            else
+            {
+                numVariant.Enabled = false;
+                numVariant.Value = item.Variant;
+            }
+        }
+
+        private void numQuantity_ValueChanged(object sender, EventArgs e)
+        {
+            if (_item != null)
+                _item.Stack = Convert.ToInt32(numQuantity.Value);
+        }
+
+        private void numQuality_ValueChanged(object sender, EventArgs e)
+        {
+            if (_item != null)
+            {
+                _item.Quality = Convert.ToInt32(numQuality.Value);
+                _item.Durability = _item.GetMaxDurability();
+            }
+        }
+
+        private void numVariant_ValueChanged(object sender, EventArgs e)
+        {
+            if (_item != null)
+            {
+                _item.Variant = Convert.ToInt32(numVariant.Value);
+                UpdateImage();
+            }
+        }
+
+        private void UpdateImage()
+        {
+            Image image = Image.FromFile(_item.GetImagePath());
+            pbItemImage.Image = image;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+
+        private void btnApply_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+    }
+}
