afce3812b23de6b67c49c4999c41b984f23124f0
Nikolas Andreou <nickandreou94@gmail.com>
2021-07-03 06:31:24 +0300

    Added about form with useful information

---
+++ ValheimCharacterEditor/Forms/FrmAbout.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace ValheimCharacterEditor.Forms
+{
+    partial class FrmAbout
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmAbout));
+            this.labelProductName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAbout = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.linkGithub = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // labelProductName
+            // 
+            this.labelProductName.AutoSize = true;
+            this.labelProductName.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelProductName.Location = new System.Drawing.Point(12, 16);
+            this.labelProductName.Name = "labelProductName";
+            this.labelProductName.Size = new System.Drawing.Size(190, 16);
+            this.labelProductName.TabIndex = 0;
+            this.labelProductName.Text = "Valheim Character Editor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Location = new System.Drawing.Point(15, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(264, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "An open source character editor for Valheim.";
+            // 
+            // txtAbout
+            // 
+            this.txtAbout.BackColor = System.Drawing.SystemColors.Window;
+            this.txtAbout.Location = new System.Drawing.Point(12, 61);
+            this.txtAbout.Multiline = true;
+            this.txtAbout.Name = "txtAbout";
+            this.txtAbout.ReadOnly = true;
+            this.txtAbout.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtAbout.Size = new System.Drawing.Size(317, 279);
+            this.txtAbout.TabIndex = 2;
+            this.txtAbout.TabStop = false;
+            this.txtAbout.Text = resources.GetString("txtAbout.Text");
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(254, 346);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // linkGithub
+            // 
+            this.linkGithub.AutoSize = true;
+            this.linkGithub.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(145)))), ((int)(((byte)(244)))));
+            this.linkGithub.Location = new System.Drawing.Point(15, 351);
+            this.linkGithub.Name = "linkGithub";
+            this.linkGithub.Size = new System.Drawing.Size(150, 13);
+            this.linkGithub.TabIndex = 4;
+            this.linkGithub.TabStop = true;
+            this.linkGithub.Text = "Official GitHub repository";
+            this.linkGithub.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkGithub_LinkClicked);
+            // 
+            // FrmAbout
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 380);
+            this.Controls.Add(this.linkGithub);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.txtAbout);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelProductName);
+            this.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FrmAbout";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "About Valheim Character Editor";
+            this.Load += new System.EventHandler(this.FrmAbout_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelProductName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAbout;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.LinkLabel linkGithub;
+    }
+}
---
+++ ValheimCharacterEditor/Forms/FrmAbout.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace ValheimCharacterEditor.Forms
+{
+    public partial class FrmAbout : Form
+    {
+        public FrmAbout()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, System.EventArgs e)
+        {
+            Close();
+        }
+
+        private void linkGithub_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://github.com/Codeh4ck/Valheim-Character-Editor");
+        }
+
+        private void FrmAbout_Load(object sender, System.EventArgs e)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+
+            labelProductName.Text += $" v{fileVersionInfo.FileMajorPart}.{fileVersionInfo.FileMinorPart}";
+
+            Focus();
+        }
+    }
+}
