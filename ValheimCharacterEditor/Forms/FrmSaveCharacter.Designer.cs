d6ed17c5b9ee97878f3a08c27698d4905c3679c8
Nikolas Andreou <nickandreou94@gmail.com>
2021-07-03 06:32:57 +0300

    Character save dialog which displays character changes

---
+++ ValheimCharacterEditor/Forms/FrmSaveCharacter.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace ValheimCharacterEditor.Forms
+{
+    partial class FrmSaveCharacter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmSaveCharacter));
+            this.labelCharacterName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtChanges = new System.Windows.Forms.RichTextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.chkBackup = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // labelCharacterName
+            // 
+            this.labelCharacterName.AutoSize = true;
+            this.labelCharacterName.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.labelCharacterName.Location = new System.Drawing.Point(120, 21);
+            this.labelCharacterName.Name = "labelCharacterName";
+            this.labelCharacterName.Size = new System.Drawing.Size(36, 13);
+            this.labelCharacterName.TabIndex = 40;
+            this.labelCharacterName.Text = "None";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 39;
+            this.label1.Text = "Character name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 41;
+            this.label2.Text = "Changes:";
+            // 
+            // txtChanges
+            // 
+            this.txtChanges.Location = new System.Drawing.Point(12, 66);
+            this.txtChanges.Name = "txtChanges";
+            this.txtChanges.Size = new System.Drawing.Size(395, 236);
+            this.txtChanges.TabIndex = 42;
+            this.txtChanges.Text = "";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(271, 308);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(136, 23);
+            this.btnSave.TabIndex = 43;
+            this.btnSave.Text = "Save character";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(185, 308);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(80, 23);
+            this.btnCancel.TabIndex = 44;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // chkBackup
+            // 
+            this.chkBackup.AutoSize = true;
+            this.chkBackup.Location = new System.Drawing.Point(12, 312);
+            this.chkBackup.Name = "chkBackup";
+            this.chkBackup.Size = new System.Drawing.Size(114, 17);
+            this.chkBackup.TabIndex = 45;
+            this.chkBackup.Text = "Backup original";
+            this.chkBackup.UseVisualStyleBackColor = true;
+            // 
+            // FrmSaveCharacter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 346);
+            this.Controls.Add(this.chkBackup);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtChanges);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelCharacterName);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmSaveCharacter";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Save character:";
+            this.Load += new System.EventHandler(this.FrmSaveCharacter_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelCharacterName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox txtChanges;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.CheckBox chkBackup;
+    }
+}
---
+++ ValheimCharacterEditor/Forms/FrmSaveCharacter.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ValheimCharacterEditor.Models;
+using ValheimCharacterEditor.Utilities;
+using ValheimCharacterEditor.Utilities.Extensions;
+using ValheimCharacterEditor.Valheim;
+
+namespace ValheimCharacterEditor.Forms
+{
+    public partial class FrmSaveCharacter : Form
+    {
+        private readonly CharacterEntity _character;
+
+        public FrmSaveCharacter(CharacterEntity character)
+        {
+            if (character == null) throw new ArgumentNullException(nameof(character));
+            _character = character;
+
+            InitializeComponent();
+        }
+
+        public static bool SaveCharacter(Form parent, CharacterEntity character)
+        {
+            using FrmSaveCharacter saveCharacter = new FrmSaveCharacter(character);
+            if (saveCharacter.ShowDialog(parent) != DialogResult.OK) return false;
+
+            return true;
+        }
+
+        private void FrmSaveCharacter_Load(object sender, EventArgs e)
+        {
+            Text = $"{Text} {_character.Data.Name}";
+            GetDifferences();
+        }
+
+        private void GetDifferences()
+        {
+            CharacterEntity original =
+                CharacterLoader.CharactersUnmodified.FirstOrDefault(x => x.Data.Id == _character.Data.Id);
+
+            if (original == null)
+            {
+                txtChanges.Text = "New character. No differences";
+                return;
+            }
+
+            labelCharacterName.Text = original.Data.Name;
+
+            DifferenceReader reader = new DifferenceReader(original.Data, _character.Data);
+            CharacterDetailsDifference differences = reader.GetCharacterDetailsChanges();
+
+            if (differences.NameDifference != null)
+            {
+                txtChanges.AppendText(differences.NameDifference.Name + "\r\n");
+                txtChanges.AppendText($"{differences.NameDifference.PreviousValue} -> {differences.NameDifference.NewValue}\r\n\r\n");
+            }
+
+            if (differences.GenderDifference != null)
+            {
+                txtChanges.AppendText(differences.GenderDifference.Name + "\r\n");
+                txtChanges.AppendText($"{differences.GenderDifference.PreviousValue} -> {differences.GenderDifference.NewValue}\r\n\r\n");
+            }
+
+            if (differences.BeardDifference != null)
+            {
+                txtChanges.AppendText(differences.BeardDifference.Name + "\r\n");
+                txtChanges.AppendText($"{differences.BeardDifference.PreviousValue} -> {differences.BeardDifference.NewValue}\r\n\r\n");
+            }
+
+            if (differences.HairDifference != null)
+            {
+                txtChanges.AppendText(differences.HairDifference.Name + "\r\n");
+                txtChanges.AppendText($"{differences.HairDifference.PreviousValue} -> {differences.HairDifference.NewValue}\r\n\r\n");
+            }
+
+            if (differences.SkinToneDifference != null)
+            {
+                txtChanges.AppendText(differences.SkinToneDifference.Name + "\r\n");
+                txtChanges.AppendText($"{differences.SkinToneDifference.PreviousValue.ToColorString()} -> {differences.SkinToneDifference.NewValue.ToColorString()}\r\n\r\n");
+            }
+
+            if (differences.HairColorDifference != null)
+            {
+                txtChanges.AppendText(differences.HairColorDifference.Name + "\r\n");
+                txtChanges.AppendText($"{differences.HairColorDifference.PreviousValue.ToColorString()} -> {differences.HairColorDifference.NewValue.ToColorString()}\r\n\r\n");
+            }
+
+            foreach (SkillDifference skillDifference in differences.SkillDifferences)
+            {
+                txtChanges.AppendText($"{skillDifference.SkillName:F}\r\n");
+                txtChanges.AppendText($"{Convert.ToInt32(skillDifference.PreviousValue)} -> {Convert.ToInt32(skillDifference.NewValue)}\r\n\r\n");
+            }
+
+            foreach (InventoryDifference inventoryDifference in differences.InventoryDifferences)
+            {
+                txtChanges.AppendText($"{inventoryDifference.Name}\r\n");
+                if (!string.IsNullOrEmpty(inventoryDifference.PreviousValue))
+                {
+                    txtChanges.AppendText("-> Previous item:\r\n");
+                    txtChanges.AppendText($"{inventoryDifference.PreviousValue}\r\n");
+                    txtChanges.AppendText("-> New item:\r\n");
+                    txtChanges.AppendText($"{inventoryDifference.NewValue}\r\n");
+                }
+                else
+                    txtChanges.AppendText($"{inventoryDifference.NewValue}\r\n\r\n");
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string oldFile = _character.File;
+
+            BackupResult backupResult = null;
+
+            if (chkBackup.Checked)
+            {
+                if (string.IsNullOrEmpty(oldFile) || !File.Exists(oldFile))
+                {
+                    DialogResult dialogResult = MessageBox.Show(
+                        "The old character file has been deleted. Backup will be skipped.\n\nWould you like to proceed?",
+                        "Old character file missing!", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+
+                    if (dialogResult == DialogResult.No) return;
+                }
+                else
+                {
+                    backupResult = BackupCharacter(oldFile);
+                }
+            }
+
+            bool saveResult = CharacterParser.SaveCharacterFile(oldFile, _character);
+
+            StringBuilder builder = new StringBuilder();
+
+            builder.Append(
+                $"Your character modifications {(saveResult ? "have been saved successfully!" : "could not be saved.")}");
+
+            if (saveResult && backupResult != null)
+            {
+                builder.AppendLine(
+                    backupResult.Successful
+                        ? $"\n\nYour character has been backed up successfully!\n\nYou can find the backup at:\n{backupResult.BackupFile}"
+                        : $"\n\nCould not backup your character.\nError message:\n\n{backupResult.BackupMessage}");
+            }
+
+            MessageBox.Show(builder.ToString(),
+                saveResult ? 
+                    "Character saved successfully!" :
+                    "Could not save character!",
+                MessageBoxButtons.OK,
+                saveResult ? 
+                    MessageBoxIcon.Information :
+                    MessageBoxIcon.Exclamation);
+            
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+
+        private BackupResult BackupCharacter(string oldFile)
+        {
+            string oldFileDirectory = Path.GetDirectoryName(oldFile);
+            string backupFilename = $"{Path.GetFileNameWithoutExtension(oldFile)}-BAK.fch";
+
+            string backupFullPath;
+
+            if (!string.IsNullOrEmpty(oldFileDirectory))
+                backupFullPath = Path.Combine(oldFileDirectory, backupFilename);
+            else
+            {
+                FolderBrowserDialog folderBrowser = new FolderBrowserDialog()
+                {
+                    Description = "Select a folder to save your character backup...",
+                    ShowNewFolderButton = true
+                };
+
+                if (folderBrowser.ShowDialog(this) != DialogResult.OK)
+                {
+                    return new BackupResult()
+                    {
+                        BackedFile = oldFile,
+                        BackupFile = string.Empty,
+                        Successful = false,
+                        BackupMessage = "User did not select a backup folder."
+                    };
+                };
+
+                backupFullPath = Path.Combine(folderBrowser.SelectedPath, backupFilename);
+            }
+
+            try
+            {
+                File.Copy(oldFile, backupFullPath);
+
+                return new BackupResult()
+                {
+                    BackupMessage = $"Backup has been completed successfully!",
+                    BackedFile = oldFile,
+                    BackupFile = backupFullPath,
+                    Successful = true
+                };
+            }
+            catch (Exception ex)
+            {
+                return new BackupResult()
+                {
+                    BackupMessage = $"Could not backup file. Error: {ex.Message}",
+                    BackedFile = oldFile,
+                    BackupFile = backupFullPath,
+                    Successful = false
+                };
+            }
+        }
+    }
+}
