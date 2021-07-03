36072b4cc9f83e05a062030fdb894d933fbfd639
Nikolas Andreou <nickandreou94@gmail.com>
2021-07-03 06:34:04 +0300

    Added Vector3 comparator method

--- ValheimCharacterEditor/Valheim/Vector3.cs
+++ ValheimCharacterEditor/Valheim/Vector3.cs
@@ -1,9 +1,31 @@
-﻿namespace ValheimCharacterEditor.Valheim
+﻿using System;
+
+namespace ValheimCharacterEditor.Valheim
 {
+#pragma warning disable CS0659
     public class Vector3
+#pragma warning restore CS0659
     {
-        public float X;
-        public float Y;
-        public float Z;
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+
+        public override bool Equals(object other)
+        {
+            if (other == null) return false;
+            if (GetType() != other.GetType()) return false;
+            if (ReferenceEquals(this, other)) return true;
+
+            return CompareProperties((Vector3) other);
+        }
+
+        private bool CompareProperties(Vector3 other)
+        {
+            if (Math.Abs(X - other.X) >= float.Epsilon) return false;
+            if (Math.Abs(Y - other.Y) >= float.Epsilon) return false;
+            if (Math.Abs(Z - other.Z) >= float.Epsilon) return false;
+
+            return true;
+        }
     }
 }
